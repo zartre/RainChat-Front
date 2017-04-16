6ef9c9f901e6d10d7f9492e5b286d27837ff81ce
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-04-16 14:32:43 +0700

    Add auto-prefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var csso = require('gulp-csso');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
 	gulp.watch('css/chat.css', ['min-style']);
 });
 
+gulp.task('prefix', function() {
+	gulp.src('css/chat.css')
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest('css/dist'))
+	console.log('Prefixed. Minifying...');
+});
+
 gulp.task('min-style', function() {
-	return gulp.src('css/chat.css')
-		.pipe(csso())
-		.pipe(gulp.dest('css/dist'));
+	return gulp.src('css/dist/chat.css')
+	.pipe(csso())
+	.pipe(gulp.dest('css/dist'));
 	console.log('Minified. Watching for changes...');
 });
