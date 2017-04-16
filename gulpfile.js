e62567bf31d3bc8ba2ebad69d4c526ad25e2c319
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-04-16 15:00:37 +0700

    Add prefixes to CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var csso = require('gulp-csso');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
-	gulp.watch('css/chat.css', ['min-style']);
+	gulp.watch('css/chat.css', ['prefix', 'min-style']);
 });
 
 gulp.task('prefix', function() {
@@ -12,12 +12,12 @@ gulp.task('prefix', function() {
 		browsers: ['last 2 versions'],
 		cascade: false
 	}))
-	.pipe(gulp.dest('css/dist'))
+	.pipe(gulp.dest('css/prefixed'));
 	console.log('Prefixed. Minifying...');
 });
 
 gulp.task('min-style', function() {
-	return gulp.src('css/dist/chat.css')
+	return gulp.src('css/prefixed/chat.css')
 	.pipe(csso())
 	.pipe(gulp.dest('css/dist'));
 	console.log('Minified. Watching for changes...');
