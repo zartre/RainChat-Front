0681865dffa9a8b974029fa9c859f1f2a0299202
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-04-21 17:15:01 +0700

    Fix Safari keyboard overlay again

--- resources/views/dev/chat.blade.php
+++ resources/views/dev/chat.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, target-densityDpi=device-dpi">
     <meta property="og:image" content="{{ asset('img/ogimage.jpg') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('favicon/favicon-32x32.png') }}" sizes="32x32">
